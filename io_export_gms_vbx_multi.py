0253c6de5a9fff1566a043fc1916de7f2ecca948
Bart Teunis <bart_teunis@hotmail.com>
2018-02-11 07:41:02 +0100

    Added several pieces of old code of which parts will be used for this new plugin

--- io_export_gms_vbx_multi.py
+++ io_export_gms_vbx_multi.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Export GM:Studio MultiTexture",
     "description": "Export the current scene to a buffer that can be loaded in GM:Studio",
     "author": "Bart Teunis",
-    "version": (2, 0, 0),
+    "version": (1, 0, 0),
     "blender": (2, 78, 0),
     "location": "File > Export",
     "warning": "", # used for warning icon and text in addons panel
@@ -16,6 +16,91 @@ import os
 from array import *
 from os import path
 from struct import pack
+
+# First preparations
+def prepare_selection():
+    bpy.ops.object.duplicates_make_real()
+    bpy.ops.object.make_local(type='SELECT_OBDATA') # TODO: also MATERIAL??
+    bpy.ops.object.make_single_user(object=True,obdata=True,material=False,texture=False,animation=False)
+    bpy.ops.object.convert(target='MESH')
+    bpy.ops.object.join()
+    bpy.ops.object.modifier_add(type='TRIANGULATE')
+    bpy.ops.object.convert(target='MESH')
+
+# Prepare vertex formats and object mappings
+def prep():
+    pass
+
+# The final function for the basic loop!
+def export():
+    # First things first: prepare selection (import from libraries, make duplis real, etc, ...
+    prepare_selection()
+
+from array import *
+from struct import pack
+from os.path import splitext
+import json
+import bpy
+
+#import logging
+
+# Return an array containing mesh data according to the given format
+# Append interpolation values when a second mesh is provided
+# PRE: vertex indices (actually loop indices) need to correspond between meshes
+# PRE: mesh should be triangulated
+def mesh_data_to_bytearray(format,added_vtx_info,mesh1,mesh2=None):
+    vertex_data = array('B')
+    
+    uvs = mesh1.uv_layers.active.data if len(mesh1.uv_layers) > 0 else None
+    
+    # Loop per vertex
+    for loop in mesh1.loops:
+        # Get the face corresponding to the loop
+        face = mesh1.polygons[loop.index // 3]                      # Assuming triangulated faces!
+        mat  = mesh1.materials[face.material_index] if len(mesh1.materials) > 0 else None
+        vtx  = mesh1.vertices[loop.vertex_index]
+        pos = vtx.co
+        nml = vtx.normal
+        col = [int(x) for x in mat.diffuse_color*255] if mat != None else [255,255,255]
+        col.reverse()                                               #bgr
+        a = mat.alpha*255
+        uv = uvs[loop.index].uv[:] if uvs != None else (0,0)
+        
+        # Current frame vertex data
+        for attrib in format:
+           if attrib == 'Pos':
+               # in_Position (x,y,z)
+                vertex_data.extend(pack('fff',*pos))
+           elif attrib == 'Normal':
+               # in_Normal (x,y,z)
+               vertex_data.extend(pack('fff',*nml))
+           elif attrib == 'Colour':
+               # in_Colour (r,g,b,a)
+               vertex_data.extend(pack('BBB',*col))                     #bgr
+           elif attrib == 'Alpha':
+                # Alpha
+               vertex_data.extend(pack('B',a))                          #a
+           elif attrib == 'Textcoord':
+               # in_TextureCoord (u,v)
+               vertex_data.extend(pack('ff',*uv))                       # Invert y textcoord
+        
+        # Next frame vertex data
+        if mesh2 != None:
+            face = mesh2.polygons[loop.index // 3]                      # Assuming triangulated faces!
+            vtx = mesh2.vertices[loop.vertex_index]
+            pos = vtx.co
+            nml = vtx.normal
+            
+            for attrib in format:
+                if attrib == 'Pos':
+                    vertex_data.extend(pack('fff',*pos))
+                elif attrib == 'Normal':
+                    vertex_data.extend(pack('fff',*nml))
+        
+        # Append additional vertex info provided by the caller
+        vertex_data.extend(added_vtx_info)
+        
+    return vertex_data
 
 # TODO: batch export objects
 # vertex index goes into vertex buffer, object index goes into json file
@@ -66,6 +151,124 @@ def get_object_data(object, filepath):
     
     return (data, result)
 
+# << Existing functionality from previous morph exporter - to be fully reworked >>
+# The core loop is important
+def write_vtx_buffer(context, filepath, caller):
+    # Easier names
+    s = context.scene
+    
+    # Remember selection
+    selection = context.selected_objects
+    active = context.active_object
+    
+    # Consider only supported object types
+    working_selection = context.selected_objects if caller.selection_only else context.scene.objects
+    working_selection = [x for x in working_selection if x.type in {'MESH'}]    # TODO: support 'EMPTY' with duplis
+    
+    # Check if working selection contains objects, cancel if not
+    if len(working_selection) == 0:
+        return {'CANCELLED'}
+    
+    # Open files
+    f_data = open(filepath, 'wb')
+    
+    # Retrieve vertex format (attributes must be in order!)
+    vertex_format = caller.vertex_format.split(',')
+    
+    # Initialize some stuff
+    b = working_selection[0]
+    old_frame = s.frame_current
+    
+    frame_range = range(s.frame_start,s.frame_end+1)
+    no_frames = len(frame_range)
+    
+    # Reset working selection
+    for obj in s.objects:
+        obj.select = True if obj in working_selection else False
+    s.objects.active = b
+    
+    s.frame_set(s.frame_start)
+    
+    # Generate first frame's 'current' mesh
+    make_merged_copy(context)
+    obj_cur = context.active_object                 # Result is now available in context.active_object
+    obj_nxt = None
+    
+    i = 0   # Frame index in vertex buffer
+    for frame in frame_range:
+        # Initialize
+        additional_vertex_info = array('B')
+        additional_vertex_info.extend(pack('f',i))
+        i += 1
+        
+        # Calculate and set next frame to be generated
+        frame_next = frame % no_frames
+        s.frame_set(frame_next)
+        
+        # Reset working selection
+        for obj in s.objects:
+            obj.select = True if obj in working_selection else False
+        s.objects.active = b
+        
+        make_merged_copy(context)
+        obj_nxt = context.active_object             # Result is now available in context.active_object
+        
+        # TODO: set object origin to (0,0,0) to allow easy scaling, scale y by -1 and flip normals
+        #context.scene.cursor_location = [0,0,0]
+        #bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+        
+        # Write current frame
+        data = mesh_data_to_bytearray(vertex_format,additional_vertex_info,obj_cur.data,obj_nxt.data) # mesh_cur and mesh_nxt should have identical loop indices and originate from the same joined meshes
+        data.tofile(f_data)
+        
+        # Delete current joined object
+        bpy.ops.object.select_pattern(pattern=obj_cur.name,extend=False)
+        bpy.ops.object.delete()
+        
+        # Make 'cur' reference the 'nxt' frame data (this frame's next is next frame's current)
+        obj_cur = obj_nxt
+    
+    # Delete last joined object
+    bpy.ops.object.select_pattern(pattern=obj_nxt.name,extend=False)
+    bpy.ops.object.delete()
+    
+    # Restore current frame
+    s.frame_set(old_frame)
+    
+    # Restore selection and active
+    for obj in selection:
+        obj.select = True
+    s.objects.active = active
+    
+    f_data.close()
+    
+    return {'FINISHED'}
+
+# TODO: replace with improved function prepare_selection()
+def make_merged_copy(context):
+    # TODO: select a root object to prevent wrong joins => much simpler: join to active object!
+    # Active object therefore must be a root object!
+    # TODO: add dupli support
+    
+    # This one is tricky
+    # When we join the exact same selection of objects in the exact same way, 
+    # we get meshes that have the same loop indices between frames
+    # This allows us to loop through the mesh's loops
+    # For a detailed background, see: https://wiki.blender.org/index.php/Dev:Source/Modeling/BMesh/Design#The_Loop_Cycle:_A_circle_of_face_edges_around_a_polygon.
+    
+    # Put all into a single mesh
+    bpy.ops.object.duplicate()                      # Duplicate selection, newly created duplicated objects become the new selection
+    #bpy.ops.object.duplicates_make_real()           # Make duplicates real (TESTING)
+    bpy.ops.object.convert(target='MESH')           # Convert to mesh, applying modifiers
+    bpy.ops.object.join()                           # Create a single mesh
+    
+    #bpy.ops.object.select_hierarchy()
+    bpy.ops.object.modifier_add(type='TRIANGULATE') # Add triangulate modifier to joined mesh (!)
+    bpy.ops.object.convert(target='MESH')           # Convert to mesh, applying modifiers
+    
+    bpy.ops.object.transform_apply(location = True, rotation = True, scale = True)
+    
+    context.active_object.animation_data_clear()    # Clear, since animation gets baked into mesh
 
 # ExportHelper is a helper class, defines filename and
 # invoke() function which calls the file selector.
